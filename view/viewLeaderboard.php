d618efeefaafb7909d698fa608f6d06e6547a1a8
D-Lilian <lilian.daurat@gmail.com>
2023-12-16 16:12:56 +0100

    putting base model for this pages

---
+++ view/viewLeaderboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Home page</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.00, user-scalable=yes" />
+        <link href="https://fonts.googleapis.com/css?family=Barlow&display=swap" rel="stylesheet" />
+		<link rel="stylesheet" href="./assets/css/home.css" />
+        <link rel="stylesheet" href="./assets/css/main.css" />
+	</head>
+    <body> 
+    <?php require('./assets/header.php'); ?>
+    <div>
+        <p>hello</p>
+    </div>
+    <?php require('./assets/footer.php'); ?>
+    </body>
+</html>
