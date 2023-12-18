400e10e78335f8633f5e50c0030b139b1761b007
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2023-12-18 17:57:32 +0100

    séparation css html

--- view/viewRegister.php
+++ view/viewRegister.php
@@ -7,66 +7,6 @@
     <link href="https://fonts.googleapis.com/css?family=Barlow&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./assets/css/register.css">
     <link rel="stylesheet" href="./assets/css/main.css">
-    <style>
-        body {
-            background: rgba(33, 31, 42, 1);
-            margin: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-        .container {
-            position: absolute;
-            width: 50%;
-            background-color: rgba(37, 40, 54, 1);
-            border-radius: 20px;
-            transform: translate(-50%, 50%);
-            padding: 20px;
-        }
-
-        .title {
-            margin-top: 0;
-            margin-bottom: 20px;
-            font-weight: bold;
-            font-size: 200%;
-        }
-
-        .info {
-            font-size: 16px;
-        }
-
-        .text {
-            width: 100%;
-            height: 40px;
-            background: rgba(43, 47, 64, 1);
-            border: none;
-            margin-bottom: 10px;
-            padding: 5px;
-            font-size: 16px;
-            border-radius: 5px;
-            color: rgba(255, 255, 255, 1);
-        }
-
-        .button {
-            width: 100%;
-            height: 40px;
-            background: rgba(103, 87, 214, 1);
-            border: none;
-            border-radius: 10px;
-            text-align: center;
-            color: rgba(255, 255, 255, 1);
-            cursor: pointer;
-        }
-
-        .login {
-            display: block;
-            margin-top: 10px;
-            color: rgba(255, 255, 255, 1);
-            text-decoration: none;
-        }
-    </style>
 </head>
 <body>
     <form method="post">
