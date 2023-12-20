296e4667231f16eb405e328fb972c40cc3c78bbc
D-Lilian <lilian.daurat@gmail.com>
2023-12-20 19:18:54 +0100

    Correcting register page

--- view/viewRegister.php
+++ view/viewRegister.php
@@ -1,17 +1,17 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Login</title>
+		<title>Register</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.00, user-scalable=yes" />
         <link href="https://fonts.googleapis.com/css?family=Barlow&display=swap" rel="stylesheet" />
-		<link rel="stylesheet" href="./assets/css/login.css" />
+		<link rel="stylesheet" href="./assets/css/register.css" />
         <link rel="stylesheet" href="./assets/css/main.css" />
 	</head>
     <body>
-        <form method="post">
-        <div class="container">
-            <h1 class="title">Modifier les informations</h1>
+    <div class="flex-container">
+        <form method="post" class="container">
+            <h1 class="title">Inscritption</h1>
             <p class="info">Nom :</p>
             <input class="text" type="text" name="lastName" value="<?php echo $_ENV["LastName"] ?>">
             <p class="info">Prénom :</p>
@@ -22,12 +22,12 @@
             <input class="text" type="password" name="password" value="">
             <p class="info">Confirmation du mot de passe :</p>
             <input class="text" type="password" name="confPassword" value="">
-            <button class="button" type="submit">Enregistrer les modifications</button>
+            <button class="button" type="submit">S'inscrire</button>
             <br>
-            <a href="./view/viewLogin.php" class="login">Se connecter</a>
-        </div>
+            <a href="./view/viewLogin.php" class="register">Se connecter</a>
         </form>
-        <?php require('./assets/footer.php'); ?>
+    </div>
+    <?php require('./assets/footer.php'); ?>
     </body>
 </html>
 
