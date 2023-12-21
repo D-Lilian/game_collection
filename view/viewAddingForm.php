be2a088fec0149cb28bd594157f9820fd406a40c
D-Lilian <lilian.daurat@gmail.com>
2023-12-21 22:49:08 +0100

    adding require for the form

--- view/viewAddingForm.php
+++ view/viewAddingForm.php
@@ -16,15 +16,15 @@
                 <p>Le jeu que vous souhaitez ajouter n'existe pas ! Vous pouvez le créer, celui-ci sera automatiquement ajouter à votre bibliothèque !</p>
                 <br><br>
                 <p class="info">Nom du jeu :</p>
-                <input class="text" type="text" name="nameGame" placeholder="Nom du jeu" minlength=1 maxlength=200>
+                <input class="text" type="text" name="nameGame" placeholder="Nom du jeu" minlength=1 maxlength=200 required>
                 <p class="info">Editeur du jeu :</p>
-                <input class="text" type="text" name="editor" placeholder="Editeur de jeu" minlength=1 maxlength=200>
+                <input class="text" type="text" name="editor" placeholder="Editeur de jeu" minlength=1 maxlength=200 required>
                 <p class="info">Sortie du jeu :</p>
-                <input class="text" type="date" name="date" >
+                <input class="text" type="date" name="date" required>
                 <h3 class="info">Platefomes :</h3>
                 <p class="info">PC :</p>
                 <label class="container">
-                    <input class="checkbox" type="checkbox" name="pc" >
+                    <input class="checkbox" type="checkbox" name="pc" checked>
                     <span class="checkmark"></span>
                 </label>
                 <p class="info">Playstation :</p>
@@ -42,12 +42,17 @@
                     <input class="checkbox" type="checkbox" name="switch" >
                     <span class="checkmark"></span>
                 </label>
+                <p class="info">Mobile :</p>
+                <label class="container">
+                    <input class="checkbox" type="checkbox" name="mobile" >
+                    <span class="checkmark"></span>
+                </label>
                 <p class="info">Description du jeu :</p>
-                <input class="description" type="textarea" name="description" placeholder="Description du jeu">
+                <input class="description" type="textarea" name="description" placeholder="Description du jeu" required>
                 <p class="info">URL de la cover :</p>
-                <input class="text" type="url" name="urlCover" placeholder="URL de la cover" minlength=1 maxlength=500>
+                <input class="text" type="url" name="urlCover" placeholder="URL de la cover" minlength=1 maxlength=500 required>
                 <p class="info">URL du site :</p>
-                <input class="text" type="url" name="urlWebsite" placeholder="URL du site" minlength=1 maxlength=500>
+                <input class="text" type="url" name="urlWebsite" placeholder="URL du site" minlength=1 maxlength=500 required>
                 <button class="button" type="submit">AJOUTER LE JEU</button>
             </form>
         </div>
