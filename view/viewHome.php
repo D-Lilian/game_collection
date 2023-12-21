5ad1413827d029ee70548bff95fe3031ff793894
D-Lilian <lilian.daurat@gmail.com>
2023-12-21 22:49:24 +0100

    reformating

--- view/viewHome.php
+++ view/viewHome.php
@@ -1,48 +1,56 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<title>Home page</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.00, user-scalable=yes" />
-        <link href="https://fonts.googleapis.com/css?family=Barlow&display=swap" rel="stylesheet" />
-		<link rel="stylesheet" href="./assets/css/home.css" />
-        <link rel="stylesheet" href="./assets/css/main.css" />
-	</head>
-    <body> 
+
+<head>
+    <title>Home page</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.00, user-scalable=yes" />
+    <link href="https://fonts.googleapis.com/css?family=Barlow&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="./assets/css/home.css" />
+    <link rel="stylesheet" href="./assets/css/main.css" />
+</head>
+
+<body>
     <?php require('./assets/header.php'); ?>
     <div class="banner">
         <br>
         <div class="nonBanner">SALUT MACHIN
-        <br>PRÊT A AJOUTER DES
-        <br>JEUX A TA COLLECTION ?</div>
+            <br>PRÊT A AJOUTER DES
+            <br>JEUX A TA COLLECTION ?
+        </div>
+    </div>
+    <div>
+
     </div>
     <div>
         <div class="myGames">
             <h1>Mes jeux</h1>
         </div>
         <div>
-		    <div class="container">
+            <div class="container">
                 <div class="row">
                     <div class="col-4">
                         <!-- Box -->
-		    			<section>
+                        <section>
                             <div class="boxcontainer">
                                 <div class="content">
                                     <div class="text">
                                         <h2>name of the game</h2>
                                         <div>
-                                            <p class="platform">Pc, Xbox serie X, Xbox serie S, Sbox one, Playstation 4, Playstation 5, Nintendo Switch, Android phone, IOS phone</p>
+                                            <p class="platform">Pc, Xbox serie X, Xbox serie S, Sbox one, Playstation 4,
+                                                Playstation 5, Nintendo Switch, Android phone, IOS phone</p>
                                             <p class="hours">6666H</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-		    			</section>
+                        </section>
                     </div>
-		    	</div>
-		    </div>
-	    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <?php require('./assets/footer.php'); ?>
-    </body>
+</body>
+
 </html>
