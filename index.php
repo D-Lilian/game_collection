d63af8016f180189d6c8356a88a37237f90c9ea5
D-Lilian <lilian.daurat@gmail.com>
2023-12-22 11:55:34 +0100

    add home page index

--- index.php
+++ index.php
@@ -4,14 +4,16 @@ require('./vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-var_dump($_GET);
-
 if($_GET['page'] === 'register'){
     require('./controller/controllerRegister.php');
 }
 
 if($_GET['page'] === 'login'){
     require('./controller/controllerLogin.php');
+}
+
+if($_GET['page'] === 'home'){
+    require('./view/viewHome.php');
 }
 
 
