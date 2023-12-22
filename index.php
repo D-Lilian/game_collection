9d03a87f45f5d6a7f973b2041f34cdbefee4df8d
D-Lilian <lilian.daurat@gmail.com>
2023-12-22 17:57:34 +0100

    updating index deplacement

--- index.php
+++ index.php
@@ -4,16 +4,16 @@ require('./vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+if($_GET['page'] === 'login' || $_GET['page'] == null || isset($_GET['page'])){
+    require('./controller/controllerLogin.php');
+}
+
 if($_GET['page'] === 'register'){
     require('./controller/controllerRegister.php');
 }
 
-if($_GET['page'] === 'login'){
-    require('./controller/controllerLogin.php');
-}
-
 if($_GET['page'] === 'home'){
-    require('./controller/controllerHome.php');
+    require('./view/viewHome.php');
 }
 
 if($_GET['page'] === 'add'){
