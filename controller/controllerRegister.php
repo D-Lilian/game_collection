1a469fef22cb5a094479efa39ea0b6c545bcae64
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2023-12-22 12:01:26 +0100

    mise a jour fin de cours 22 decembre register fini sans insertion

--- controller/controllerRegister.php
+++ controller/controllerRegister.php
@@ -3,7 +3,9 @@
 var_dump($_POST);
 if (isset($_POST['email'])){ 
     
+    require("./model/modelGameCollection.php");
     //appel du modèle pour check si mail uti already used
+    
     //appel du modèle pour insertion en db
 
     session_start();
@@ -13,20 +15,15 @@ if (isset($_POST['email'])){
     $nom = $_POST['lastName'];
     $prenom = $_POST['firstName'];
     $Mail_Uti = $_POST['email'];
-    if (true){
-        $_SESSION ["Mail_Uti"]=$Mail_Uti;
-        }
+    if (isAPlayerInDataBase($Mail_Uti)){//il y a deja un compte mais on gere pas encore le cas ou il a deja un compte
+        require('./view/viewRegister.php');
+    }else {
+
+    $_SESSION ["Mail_Uti"]=$Mail_Uti;    
     $_GET['page']="login";
     header('index.php/login');
-}else {
-    require('./view/viewRegister.php');
 }
- 
-
-
-
-//appel du modèle pour check si mail uti already used
-//appel du modèle pour insertion en db
+}
 
 
 
