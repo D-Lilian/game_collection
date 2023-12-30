092b11da21e5008b48b3af925a8c47e3863e31a9
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-12-30 18:42:53 +0100

    go to login
    alway go to login when enter localhost only

--- index.php
+++ index.php
@@ -4,36 +4,40 @@ require("vendor/autoload.php");
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-if($_GET['page'] === 'register'){
-    require('./controller/controllerRegister.php');
-}
-
-if($_GET['page'] === 'login'){
+if (!isset($_GET['page'])){
     require('./controller/controllerLogin.php');
 }
+else{
+    if($_GET['page'] === 'register'){
+        require('./controller/controllerRegister.php');
+    }
 
-if($_GET['page'] === 'home'){
-    require('./controller/controllerHome.php');
+    if($_GET['page'] === 'login'){
+        require('./controller/controllerLogin.php');
+    }
+
+    if($_GET['page'] === 'home'){
+        require('./controller/controllerHome.php');
+    }
+
+    if($_GET['page'] === 'add'){
+        require('./controller/controllerAdding.php');
+    }
+
+    if($_GET['page'] === 'addgame'){
+        require('./controller/controllerAddingForm.php');
+    }
+
+    if($_GET['page'] === 'profil'){
+        require('./controller/controllerProfil.php');
+    }
+
+    if($_GET['page'] === 'update'){
+        require('./controller/controllerUpdate.php');
+    }
+
+    if($_GET['page'] === 'leaderboard'){
+        require('./controller/controllerLeaderboard.php');
+    }
 }
-
-if($_GET['page'] === 'add'){
-    require('./controller/controllerAdding.php');
-}
-
-if($_GET['page'] === 'addgame'){
-    require('./controller/controllerAddingForm.php');
-}
-
-if($_GET['page'] === 'profil'){
-    require('./controller/controllerProfil.php');
-}
-
-if($_GET['page'] === 'update'){
-    require('./controller/controllerUpdate.php');
-}
-
-if($_GET['page'] === 'leaderboard'){
-    require('./controller/controllerLeaderboard.php');
-}
-
 ?>
