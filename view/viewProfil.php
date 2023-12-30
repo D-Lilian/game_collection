ba15c09961f4cd874df9e64173b276ed06325ecb
JDjudgedead4 <88926501+JDjudgedead4@users.noreply.github.com>
2023-12-30 13:30:46 +0100

    modification vue profil champ auto remplis

--- view/viewProfil.php
+++ view/viewProfil.php
@@ -16,20 +16,20 @@
         <form method="post">
             <h1 class="title">Mon profil</h1>
             <p class="info">Nom :</p>
-            <input class="text" type="text" name="lastName" value="<?php echo $_ENV["LastName"] ?>" minlength=1
+            <input class="text" type="text" name="lastName" value="<?php echo $_SESSION["LastName"] ?>" minlength=1
                 maxlength=100>
             <p class="info">Prénom :</p>
-            <input class="text" type="text" name="firstName" value="<?php echo $_ENV["FirstName"] ?>" minlength=1
+            <input class="text" type="text" name="firstName" value="<?php echo $_SESSION["FirstName"] ?>" minlength=1
                 maxlength=100>
             <p class="info">Email :</p>
-            <input class="text" type="text" name="email" value="<?php echo $_ENV["mail"] ?>" minlength=1 maxlength=500>
+            <input class="text" type="text" name="email" value="<?php echo $_SESSION["mail"] ?>" minlength=1 maxlength=500>
             <p class="info">Mot de passe :</p>
             <input class="text" type="password" name="password" value="" minlength=1 maxlength=100>
             <p class="info">Confirmation du mot de passe :</p>
             <input class="text" type="password" name="confPassword" value="" minlength=1 maxlength=100>
-            <button class="update" type="submit">MODIFIER</button>
-            <button class="disconnect" type="submit">SE DÉCONNECTER</button>
-            <button class="delete" type="submit">SUPPRIMER MON COMPTE</button>
+            <button class="update" name="update" value="yes" type="submit">MODIFIER</button>
+            <button class="disconnect" name="disconnect" value="yes" type="submit">SE DÉCONNECTER</button>
+            <button class="delete" name="delete" value="yes" type="submit">SUPPRIMER MON COMPTE</button>
         </form>
     </div>
     <?php require('./assets/footer.php'); ?>
